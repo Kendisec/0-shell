b4a63dd9c1102b2b8441fa460051104de3647107
edieng <emmanueldieng294@gmail.com>
2024-12-04 12:41:56 +0000

    add command pwd and cd

--- src/core/executor.rs
+++ src/core/executor.rs
@@ -1,3 +1,5 @@
+use crate::commands::{cd::{change_directory, get_home_dir}, pwd::get_current_directory};
+
 use super::interpreter::Command;
 use anyhow::Result;
 
@@ -11,7 +13,35 @@ impl Executor {
     pub fn execute(&self, command: Command) -> Result<()> {
         match command.name.as_str() {
             "" => Ok(()), // Empty command
-            "exit" => Ok(()),
+            "exit" => Ok(()), // Exit command
+            "pwd" => {
+                match get_current_directory() {
+                    Ok(dir) => {
+                        println!("{}", dir);
+                        Ok(())
+                    }
+                    Err(err) => Err(anyhow::anyhow!("Error executing pwd: {}", err)),
+                }
+            }
+            "cd" => {
+                // Ensure a path is provided as an argument
+                if let Some(path) = command.args.get(0) {
+                    match change_directory(path) {
+                        Ok(()) => Ok(()),
+                        Err(_err) => Err(anyhow::anyhow!("cd: no such file or directory: {}", path)),
+                    }
+                } else {
+                    let home = get_home_dir();
+                    if let Some(path) = home{
+                        match change_directory(&path) {
+                            Ok(()) => Ok(()),
+                            Err(_err) => Err(anyhow::anyhow!("cd: no such file or directory {}", path)),
+                        }
+                    }else{
+                        Err(anyhow::anyhow!("cd: no such file or directory "))
+                    }
+                }
+            }
             cmd => Err(anyhow::anyhow!("Command '{}' not found", cmd)),
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use shell::core::shell::Shell;
 
-
-
 fn main() -> anyhow::Result<()> {
     let mut shell = Shell::new();
     shell.run()
