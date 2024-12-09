6f0c6355f09e55d7d7966d4e387c4ac6f006d524
jgoudiab <jeangabrielgoudiaby@gmail.com>
2024-12-09 17:11:25 +0000

    commande cp

--- src/commands/echo.rs
+++ src/commands/echo.rs
@@ -11,7 +11,10 @@ pub fn echo(args: Vec<String>, variables: &HashMap<String, String>) {
     let mut filtre_arg = Vec::new();
     for arg in args {
         let handled_arg = handle_variables(&arg, variables);
-        let filtered_arg = handled_arg.replace("\"", "");
+        let filtered_arg = handled_arg.replace("\"", "")
+                                      .replace("\\n", "\n")
+                                      .replace("\\t", "\t")
+                                      .replace("\\r", "\r");
         filtre_arg.push(filtered_arg);
     }
 
